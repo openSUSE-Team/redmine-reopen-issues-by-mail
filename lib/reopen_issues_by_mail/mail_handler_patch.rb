821a1e43256a3ea625bb9355e97c070ee95fd060
Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
2023-07-23 03:50:46 +0200

    Update for Redmine 5
    This imports the code from
    https://www.redmine.org/boards/2/topics/59807 with minor cosmetical
    changes.
    
    Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

--- lib/reopen_issues_by_mail/mail_handler_patch.rb
+++ lib/reopen_issues_by_mail/mail_handler_patch.rb
@@ -3,7 +3,8 @@ module ReopenIssuesByMail
     def self.included(base)
       base.send(:include, InstanceMethods)
       base.class_eval do
-        alias_method_chain :receive_issue_reply, :reopen_issues_by_mail
+        alias_method :receive_issue_reply_without_reopen_issues_by_mail, :receive_issue_reply
+        alias_method :receive_issue_reply, :receive_issue_reply_with_reopen_issues_by_mail
       end
     end
 
@@ -14,11 +15,11 @@ module ReopenIssuesByMail
         issue = Issue.find_by_id(issue_id)
         return unless issue
         if issue.closed?
-          status_id = IssueStatus.default.id
+          status_id = 1 # ID of the status you want to set (find it in the Administration panel by inspecting the hyperlink of the desired status under "Issue statuses")
           JournalDetail.create(:journal => journal, :property => "attr",
                                :prop_key => "status_id", :value => status_id,
                                :old_value => issue.status_id)
-          Issue.where(:id => issue.id).update_all({:status_id => status_id})
+          Issue.where(:id => issue_id).update_all(:status_id => status_id)
           logger.info "MailHandler: reopening issue ##{issue.id}" if logger
         end
         journal.reload
